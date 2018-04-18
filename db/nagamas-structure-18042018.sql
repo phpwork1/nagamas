855924f70e358d5cfa84f7469ad3bd708276a7a2
phpwork1 <joshuasaputra77@gmail.com>
2018-04-18 21:38:46 +0700

    add db

---
+++ db/nagamas-structure-18042018.sql
@@ -0,0 +1,125 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.15 
+MySQL - 5.5.5-10.1.26-MariaDB : Database - nagamas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `area` */
+
+DROP TABLE IF EXISTS `area`;
+
+CREATE TABLE `area` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `a_name` varchar(100) NOT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `bam` */
+
+DROP TABLE IF EXISTS `bam`;
+
+CREATE TABLE `bam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `driver_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `b_date` date NOT NULL,
+  `b_price` int(11) NOT NULL,
+  `b_bruto` int(11) NOT NULL,
+  `b_tarra` int(11) NOT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_bam_car` (`car_id`),
+  KEY `FK_bam_driver` (`driver_id`),
+  KEY `FK_bam_area` (`area_id`),
+  CONSTRAINT `FK_bam_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_bam_car` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_bam_driver` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `car` */
+
+DROP TABLE IF EXISTS `car`;
+
+CREATE TABLE `car` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_name` varchar(100) DEFAULT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `driver` */
+
+DROP TABLE IF EXISTS `driver`;
+
+CREATE TABLE `driver` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `d_name` varchar(100) DEFAULT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `pal` */
+
+DROP TABLE IF EXISTS `pal`;
+
+CREATE TABLE `pal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `driver_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `p_date` date NOT NULL,
+  `p_price` int(11) NOT NULL,
+  `p_bruto` int(11) NOT NULL,
+  `p_tarra` int(11) NOT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_pal` (`driver_id`),
+  KEY `FK_pal_car` (`car_id`),
+  KEY `FK_pal_area` (`area_id`),
+  CONSTRAINT `FK_pal` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_pal_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_pal_car` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `palm` */
+
+DROP TABLE IF EXISTS `palm`;
+
+CREATE TABLE `palm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `factory` varchar(20) NOT NULL,
+  `p_date` date NOT NULL,
+  `p_price` int(11) NOT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
